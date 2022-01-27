bc31b72d5bf54378cfb016d3497b80c2c5bb57d5
lordzebu <lordzebu@naver.com>
2022-01-27 17:52:25 +0900

    pointer등

--- banking/banking.go
+++ banking/banking.go
@@ -1,11 +1,63 @@
 package banking
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Account struct
-type Account struct {
-	//대문자여야 다른곳에서 사용 가능. public.
-	// owner   string
-	// balance int
-	Owner   string
-	Balance int
-	//소문자는 private라서 다른곳에서 못씀..
+// type Account struct {
+// 	Owner   string
+// 	Balance int
+// }
+
+//대문자여야 다른곳에서 사용 가능. public.
+// owner   string
+// balance int
+//소문자는 private라서 다른곳에서 못씀..
+
+type Account struct {	
+	owner   string
+	balance int	
+}
+
+var errNoMoney = errors.New("Can't Withdraw")
+
+func NewAccount(owner string) *Account{ //Account를 살펴봄..
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+
+//receiver
+//Depodit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+//Balance of your Account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+//Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error{
+	if a.balance < amount{
+		return errNoMoney
+	}	 
+	a.balance -= amount
+	return nil
+}
+
+//ChangeOwner of the account
+func (a *Account) ChangeOwner (newOwner string){
+	a.owner = newOwner
+}
+
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"banking/banking"
 	"fmt"
+	"log"
 )
 
 //banking account export 방법.
 
 func main() {
-	account :=
-	fmt.Println(account, "왜안나와")
+	account := banking.NewAccount("js")	
+	account.Deposit(10)	
+	err := account.Withdraw(20)
+
+	if err != nil {
+		log.Println(err)
+		//fatal은 프로그램종료 역할
+	} 
+	fmt.Println(account.Balance(), account.Owner(), account)
 }
